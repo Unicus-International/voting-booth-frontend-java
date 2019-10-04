84da39f772070381ee7042b1fbdda2f974ff6f95
Karl <karlwesselfrogner@gmail.com>
2019-10-04 16:05:33 +0200

    wip

--- src/Controller/BallotController.java
+++ src/Controller/BallotController.java
@@ -1,8 +1,12 @@
 package Controller;
 
+import View.BallotView;
+
 public class BallotController extends AbstractController {
 
     private void getBallots(){
+        BallotView view = new BallotView();
+
 
     }
 
--- src/VotingMachine.java
+++ src/VotingMachine.java
@@ -1,13 +1,46 @@
 import View.*;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ItemListener;
+
 public class VotingMachine {
 
 
-    public static void main(String[] args) {
-        Router router = new Router();
+    public void main(String[] args) {
+        JPanel cards; //a panel that uses CardLayout
+        String BUTTONPANEL = "Card with JButtons";
+        String TEXTPANEL = "Card with JTextField";
+
+        JFrame frame = new JFrame("CardLayoutDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel comboBoxPane = new JPanel(); //use FlowLayout
+        String comboBoxItems[] = { BUTTONPANEL, TEXTPANEL };
+        JComboBox cb = new JComboBox(comboBoxItems);
+        cb.setEditable(false);
+        cb.addItemListener((ItemListener) this);
+        comboBoxPane.add(cb);
+
+        //Create the "cards".
+        JPanel card1 = new JPanel();
+        card1.add(new JButton("Button 1"));
+        card1.add(new JButton("Button 2"));
+        card1.add(new JButton("Button 3"));
+
+        JPanel card2 = new JPanel();
+        card2.add(new JTextField("TextField", 20));
+
+        //Create the panel that contains the "cards".
+        cards = new JPanel(new CardLayout());
+        cards.add(card1, BUTTONPANEL);
+        cards.add(card2, TEXTPANEL);
 
 
 
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
     }
 
 }
